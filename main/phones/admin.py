d9cfd84ba28c531d60022644dcd8dab86adec82e
Alan <alan.zybtsev@mail.ru>
2023-10-29 15:46:05 +0400

    initialize_1

--- main/phones/admin.py
+++ main/phones/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Phone
 
 # Register your models here.
+
+@admin.register(Phone)
+class SiteAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'price', 'release_date', 'lte_exists']
+    list_filter = ['name', 'price', 'lte_exists']
